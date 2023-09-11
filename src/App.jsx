7b22a4b24f07e22afda95391f2bd89574c259ca2
Jhonatan Augusto <jhonatanaugusto4@gmail.com>
2023-09-11 14:05:13 -0300

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -83,21 +83,20 @@ function winnerCalculation(squareList){
 }
 
 function App(){
-  const [xIsNext, setXIsNext] = useState(true)
+
   const [history, setHistory] = useState([Array(9).fill(null)])
   const [currentMove, setCurrentMove] = useState(0)
-  const currentSquares = history[currentMove];
+  const currentSquares = history[currentMove]
+  const xIsNext = currentMove % 2 === 0
 
   function handlePlay(nextSquares){
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
     setHistory(nextHistory)
     setCurrentMove(nextHistory.length - 1)
-    setXIsNext(!xIsNext)
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squareList, index) => {
